6fcb00e041fd5b4a1f53caa349283920963b42ce
Durban G. Keeler <durban.keeler@gmail.com>
2020-04-02 14:18:43 -0600

    Expands to county-level analysis; adds county-level population data

--- src/covid.py
+++ src/covid.py
@@ -19,132 +19,162 @@ results = dw.query(
 	'covid-19-data-resource-hub/covid-19-case-counts', 
     'SELECT * FROM covid_19_cases')
 results_df = results.dataframe.drop(
-        ['prep_flow_runtime', 'latest_date', 'difference', 'location'], axis="columns")
+        ['prep_flow_runtime', 'difference', 'location'],
+        axis="columns")
 results_df = (
-    results_df[results_df["table_names"]=="Time Series"]
+    results_df[results_df["table_names"]=="Daily Summary"]
     .drop(["table_names"], axis='columns')
 )
 results_df['date'] = pd.to_datetime(results_df['date'])
 
 
 
-world_df = results_df.pivot_table(
-    values='cases', index=
-    ['country_region', 'province_state','lat', 'long', 'date'], columns='case_type').reset_index().rename(
-        columns={'Confirmed': 'Infected', 'Deaths': 'Dead'})
-
-
-countries = gpd.read_file(
-    gpd.datasets.get_path('naturalearth_lowres')).drop(
-        ['continent', 'iso_a3'], axis='columns')
-
-world_gdf = gpd.GeoDataFrame(
-    world_df.drop(['lat', 'long'], axis='columns'), 
-    geometry=gpd.points_from_xy(world_df.long, world_df.lat), 
-    crs=countries.crs)
-world_gdf = world_gdf.loc[
-    world_gdf['date']==max(world_gdf.date)].drop('date', axis=1)
-world_gdf = world_gdf.dissolve(
-    by='country_region', aggfunc='sum')
-
-
-world_map = gpd.sjoin(
-    countries, world_gdf, how='inner', op='contains')
-world_map = world_map[
-    ['index_right', 'pop_est', 'gdp_md_est', 
-    'Infected', 'Dead', 'geometry']].rename(
-        {'index_right': 'Country', 'gdp_md_est': 'GDP'})
-
-
-fig, ax = plt.subplots(1, 1)
-world_map.plot(
-    column='Infected', ax=ax, legend=True, 
-    legend_kwds={'label': f"Infections by state (as of {max(world_df['date'])})", 
-    'orientation': 'horizontal'}
-)
-
-fig, ax = plt.subplots(1, 1)
-world_map.plot(
-    column='Dead', ax=ax, legend=True, 
-    legend_kwds={'label': f"Deaths by state (as of {max(world_df['date'])})", 
-    'orientation': 'horizontal'}
-)
 
 
 
 
-
-
+### US STATISTICS (county level)
 
 states_shp = gpd.read_file(ROOT_DIR.joinpath(
     "data/tl_2017_us_states/tl_2017_us_state.shp"))
 states_shp = states_shp[["NAME", "geometry"]]
 
-states_df = (
-    world_df[world_df["country_region"] == "US"].drop(
-        ["country_region"], axis="columns")
+# County-level shapefiles downloaded from 
+# (https://catalog.data.gov/dataset/tiger-line-shapefile-2017-nation-u-s-current-county-and-equivalent-national-shapefile)
+counties_shp = gpd.read_file(ROOT_DIR.joinpath(
+    "data/tl_2017_us_counties/tl_2017_us_county.shp"))
+
+# County population data downloaded from 
+# (https://www.census.gov/data/datasets/time-series/demo/popest/2010s-counties-total.html#par_textimage_70769902)
+county_pop = pd.read_csv(ROOT_DIR.joinpath(
+    "data/co-est2019-alldata.csv"), usecols=[3, 4, 5, 6, 18])
+
+GEOID = []
+for i,row in county_pop.iterrows():
+    GEOID.append(
+        str(row.STATE).zfill(2) + str(row.COUNTY).zfill(3))
+county_pop['GEOID'] = pd.Series(GEOID, dtype=str)
+county_pop = county_pop[
+    ['GEOID','STNAME','CTYNAME','POPESTIMATE2019']].rename(
+        columns={'POPESTIMATE2019': 'Population'})
+
+states_tmp = results_df[
+    results_df['country_region']=='US'].drop(
+        'country_region', axis='columns')
+states_df = (states_tmp.pivot_table(
+    values='cases', index=[
+        'province_state', 'admin2', 'fips', 
+        'lat', 'long', 'date'], columns='case_type')
+        .reset_index().rename(
+            columns={'Confirmed':'Infected', 'Deaths':'Dead'})
 )
-states_df = states_df.merge(
-    states_shp, how='left', 
-    left_on='province_state', right_on='NAME').filter(
-        items=['NAME', 'date', 'Infected', 'Dead', 
-        'geometry']).rename(columns={"NAME": 'Name'})
 
+states_df['fips'] = [el.zfill(5) for el in states_df.fips.astype(int).astype(str)]
+us_cases = states_df[
+    ['fips', 'province_state', 'admin2', 'date', 'Infected', 
+    'Dead', 'lat', 'long']].rename(columns={'fips': 'GEOID', 
+    'province_state': 'State', 'admin2': 'County', 
+    'date': 'Date'})
 
+us_cases = us_cases.merge(
+    county_pop, how='left', on='GEOID').assign(Infected_1k=
+        lambda x: x.Infected / x.Population * 1000)
+    
+us_cases = us_cases.filter(items=['GEOID', 'State', 'County', 
+    'Date', 'Population', 'Infected', 'Infected_1k', 'Dead'])
+county_geo = counties_shp[['GEOID', 'geometry']]
 
-states_now = gpd.GeoDataFrame(
-    states_df.loc[states_df['date'] == max(states_df['date'])]
-    .drop(['date', 'geometry'], axis='columns').reset_index()
-    .merge(states_shp, how='left', left_on='Name', 
-        right_on='NAME').drop(["NAME", 'index'], 
-        axis='columns'), 
-    crs=states_shp.crs)
+us_gdf = gpd.GeoDataFrame(
+    us_cases.merge(county_geo, how='left', on='GEOID'), 
+    geometry='geometry', crs=county_geo.crs)
 
+us_latest = us_gdf.loc[
+    us_gdf['Date']==max(us_gdf['Date'])].drop(['Date'],axis=1)
 
 plt_bnds = gpd.GeoSeries([Polygon(
         zip([-126, -126, -66, -66], [24, 50, 50, 24]))], 
-    crs=states_shp.crs)
-states_map = states_now[states_now.within(plt_bnds.iloc[0])]
-
-
-
+    crs=us_latest.crs)
+us_map = us_latest[us_latest.within(plt_bnds.iloc[0])]
 
 fig, ax = plt.subplots(1, 1)
-states_map.plot(
-    column='Infected', ax=ax, legend=True, 
-    legend_kwds={'label': f"Infections by state (as of {max(states_df['date'])})", 
-    'orientation': 'horizontal'}
-)
+us_map.plot(
+    column='Infected_1k', ax=ax, legend=True, 
+    legend_kwds={'label': f"Infections per 1k people by county (as of {max(us_cases['Date'])})", 
+    'orientation': 'horizontal'})
 
 fig, ax = plt.subplots(1, 1)
-states_map.plot(
+us_map.plot(
     column='Dead', ax=ax, legend=True, 
-    legend_kwds={'label': f"Deaths by state (as of {max(states_df['date'])})", 
-    'orientation': 'horizontal'}
-)
-
-
-states_subset = [
-    'New York', 'California', 'Washington', 
-    'District of Columbia', 'Utah', 'Texas'
-    ]
-states_ts = states_df.loc[
-    (states_df['Name'].isin(states_subset)) & 
-    (states_df['date'] > "2020-02-21")]
-
-
-fig, ax = plt.subplots()
-
-for name, group in states_ts.groupby('Name'):
-    group.plot(x='date', y='Infected', ax=ax, label=name)
-plt.yscale('log')
-plt.ylabel('Infected individuals')
-plt.xlabel('Date')
+    legend_kwds={'label': f"Deaths by county (as of {max(us_cases['Date'])})", 'orientation': 'horizontal'})
 
 
 
 
 
+# states_subset = [
+#     'New York', 'California', 'Washington', 
+#     'District of Columbia', 'Utah', 'Texas'
+#     ]
+# states_ts = states_df.loc[
+#     (states_df['Name'].isin(states_subset)) & 
+#     (states_df['date'] > "2020-02-21")]
 
 
+# fig, ax = plt.subplots()
+
+# for name, group in states_ts.groupby('Name'):
+#     group.plot(x='date', y='Infected', ax=ax, label=name)
+# plt.yscale('log')
+# plt.ylabel('Infected individuals')
+# plt.xlabel('Date')
+
+# states_group = states_df.groupby('Name')
+
+
+
+### WORLD STATISTICS
+
+# world_df = results_df.pivot_table(
+#     values='cases', index=[
+#         'country_region', 'province_state', 'admin2', 
+#         'lat', 'long', 'date'], 
+#         columns='case_type').reset_index().rename(
+#         columns={'Confirmed': 'Infected', 'Deaths': 'Dead'})
+
+
+# countries = gpd.read_file(
+#     gpd.datasets.get_path('naturalearth_lowres')).drop(
+#         ['continent', 'iso_a3'], axis='columns')
+
+# world_gdf = gpd.GeoDataFrame(
+#     world_df.drop(['lat', 'long'], axis='columns'), 
+#     geometry=gpd.points_from_xy(world_df.long, world_df.lat), 
+#     crs=countries.crs)
+# world_gdf = world_gdf.loc[
+#     world_gdf['date']==max(world_gdf.date)].drop('date', axis=1)
+# world_gdf = world_gdf.dissolve(
+#     by='country_region', aggfunc='sum')
+
+
+# world_map = gpd.sjoin(
+#     countries, world_gdf, how='inner', op='contains')
+# world_map = world_map[
+#     ['index_right', 'pop_est', 'gdp_md_est', 
+#     'Infected', 'Dead', 'geometry']].rename(
+#         {'index_right': 'Country', 'gdp_md_est': 'GDP'})
+
+
+# fig, ax = plt.subplots(1, 1)
+# world_map.plot(
+#     column='Infected', ax=ax, legend=True, 
+#     legend_kwds={'label': f"Infections by state (as of {max(world_df['date'])})", 
+#     'orientation': 'horizontal'}
+# )
+
+# fig, ax = plt.subplots(1, 1)
+# world_map.plot(
+#     column='Dead', ax=ax, legend=True, 
+#     legend_kwds={'label': f"Deaths by state (as of {max(world_df['date'])})", 
+#     'orientation': 'horizontal'}
+# )
 
